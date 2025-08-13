d476b01fabfe06db573d41770ef51b8c0dfcfe94
Dhanglerson Barral <dhanglerson.dev@outlook.com>
2025-08-13 16:05:48 -0300

    Criando CRUD de usuários

--- src/main/java/com/alunoonline/api/controller/AlunoController.java
+++ src/main/java/com/alunoonline/api/controller/AlunoController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
@@ -13,9 +16,33 @@ public class AlunoController {
     @Autowired
     AlunoService alunoService;
 
-    @PostMapping("/criar")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAluno(@RequestBody Aluno aluno) {
         alunoService.criarAluno(aluno);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Aluno> listarTodosAlunos() {
+        return alunoService.listarTodosAlunos();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Aluno> buscarAlunoPorId(@PathVariable Long id) {
+        return alunoService.buscarAlunoPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarAluno(@PathVariable Long id, @RequestBody Aluno aluno) {
+        alunoService.atualizarAluno(id, aluno);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarAluno(@PathVariable Long id) {
+        alunoService.deletarAluno(id);
     }
 }
--- src/main/java/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -4,7 +4,9 @@ import com.alunoonline.api.model.Aluno;
 import com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlunoService {
@@ -14,5 +16,35 @@ public class AlunoService {
 
     public void criarAluno(Aluno aluno) {
         alunoRepository.save(aluno);
+    }
+
+    public List<Aluno> listarTodosAlunos() {
+        return alunoRepository.findAll();
+    }
+
+    public Optional<Aluno> buscarAlunoPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public void atualizarAluno(Long id, Aluno alunoAtualizado) {
+        Optional<Aluno> alunoOptional = alunoRepository.findById(id);
+
+        if (alunoOptional.isPresent()) {
+            Aluno alunoExistente = alunoOptional.get();
+
+            if (alunoAtualizado.getNome() != null) {
+                alunoExistente.setNome(alunoAtualizado.getNome());
+            }
+
+            if (alunoAtualizado.getEmail() != null) {
+                alunoExistente.setEmail(alunoAtualizado.getEmail());
+            }
+
+            alunoRepository.save(alunoExistente);
+        }
+    }
+
+    public void deletarAluno(Long id) {
+        alunoRepository.deleteById(id);
     }
 }
